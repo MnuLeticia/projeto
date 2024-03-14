fac052b79fe8257d394499fb3e3e0b712ca46b10
“MnuLeticia” <le.moreira99@hotmail.com>
2024-03-14 14:32:19 -0300

    avatar replacement

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-lightL.png")
   } else {
-    img.setAttribute("src", "./assets/avata.png", "alt", "Foto escuro")
+    img.setAttribute("src", "./assets/avatarL.png", "alt", "Foto escuro")
   }
 }
